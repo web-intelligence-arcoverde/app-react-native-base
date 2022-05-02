ac954ed8ea225fb8cf084627883ce9a81e07c85f
Lucas Dev React Native <lucasdevreactnative@Mac-Pro-de-Lucas.local>
2022-05-02 20:24:50 -0300

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,46 @@
+import ArrowLeft from './icons/arrow-left.svg';
+import ArrowRight from './icons/arrow-right.svg';
+import Ticket from './icons/ticket.svg';
+import Cancel from './icons/cancel.svg';
+import Card from './icons/card.svg';
+import CheckCircle from './icons/check_circle.svg';
+import EyeClose from './icons/eye-close.svg';
+import EyeOpen from './icons/eye-open.svg';
+import EyeOpenError from './icons/eye-open-error.svg';
+import History from './icons/history.svg';
+import Home from './icons/home.svg';
+import Pix from './icons/pix.svg';
+import Leave from './icons/leave.svg';
+import Tariff from './icons/tariff.svg';
+import Time from './icons/time.svg';
+
+import ArrowLeftGreen from './icons/arrow-left-green.svg';
+
+import Pending from './icons/pending.svg';
+import Confirmation from './icons/confirmation.svg';
+import CancelPayment from './icons/cancel_payment.svg';
+
+import CreditCard from './icons/payment.svg';
+
+export const Icons = {
+  'arrow-left': ArrowLeft,
+  'arrow-right': ArrowRight,
+  'ticket-icon': Ticket,
+  'cancel-icon': Cancel,
+  'card-icon': Card,
+  'check-circle': CheckCircle,
+  'eye-close': EyeClose,
+  'eye-open': EyeOpen,
+  'eye-open-error': EyeOpenError,
+  'history-icon': History,
+  'home-icon': Home,
+  'pix-icon': Pix,
+  'leave-icon': Leave,
+  'tariff-icon': Tariff,
+  'time-icon': Time,
+  'arrow-left-green': ArrowLeftGreen,
+  'pending-icon': Pending,
+  'cancel-payment-icon': CancelPayment,
+  'confirmation-payment-icon': Confirmation,
+  'credit-card-icon': CreditCard,
+};
